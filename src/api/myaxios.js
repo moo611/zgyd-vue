9f78fc63fddbee33558ab975a3b9e250da3174c6
moo611 <lnpjjds@gmail.com>
2023-09-19 17:22:16 +0800

    fix

--- src/api/myaxios.js
+++ src/api/myaxios.js
@@ -1,9 +1,13 @@
 
 import axios from "axios";
 
+// let myaxios = axios.create({
+//   baseURL: 'http://localhost:5173/api',
+//   timeout: 3000,
+// });
 let myaxios = axios.create({
-  baseURL: 'http://localhost:5173/api',
+  baseURL: 'http://8.130.95.118:80',
   timeout: 3000,
-});
+})
 
 export default myaxios
